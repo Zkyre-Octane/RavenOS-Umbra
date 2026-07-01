99106a04e071f7537279250c4df2c4b3c58f4cef
Mauro Rocha <zkyre@icloud.com>
2026-06-30 17:59:40 -0700

    Delete src/raven_ui.h

--- src/raven_ui.h
+++
@@ -1,5 +0,0 @@
-// Menu functions
-    void ravenMenu(const char* options[], int numOptions, int selected); // Show menu with options
-    void ravenDialog(const char* title, const char* message); // show dialog box with title and message
-    void ravenProgressBar(int progress); // Show progress bar with current and max progress
-    void ravenStatusBar(const char* status); // Show status bar at the bottom of the screen
